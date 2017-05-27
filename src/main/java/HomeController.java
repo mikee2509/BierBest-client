1e2bee755f46fc99c3d9d28c4748a8c2afbbf32f
Michał Sieczkowski <mikee2509@gmail.com>
2017-05-27 12:46:14 +0200

    Create search result fragment

--- src/main/java/HomeController.java
+++ src/main/java/HomeController.java
@@ -1,11 +1,22 @@
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+
+
 public class HomeController {
     private Stage root;
+
+    @FXML
+    private VBox searchResults;
 
     public HomeController() {
     }
@@ -14,8 +25,16 @@ public class HomeController {
         this.root = root;
     }
 
-    public void searchBeers(ActionEvent actionEvent) {
-        //VBox vBox = (VBox) root.getScene().lookup("#search_results");
+    public void searchBeers(ActionEvent actionEvent) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fragment_search_result.fxml"));
+        AnchorPane singleResult = (AnchorPane) fxmlLoader.load();
+        ImageView beerIcon = (ImageView) singleResult.lookup("#beerIcon");
+        beerIcon.setImage(new Image("img/testimg.png"));
+        searchResults.getChildren().add(singleResult);
+        Region margin = new Region();
+        margin.setMinHeight(10);
+        searchResults.getChildren().add(margin);
+
 
     }
 }
